e560e07ed091332a664a6ac740203ab9229a4896
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-04-30 15:08:57 -0400

    [ROLES] Added stemcell role

--- roles/stemcell.rb
+++ roles/stemcell.rb
@@ -1,7 +1,7 @@
-name "base_example"
-description "Example base role applied to all nodes."
+name "stemcell"
+description "Base role applied to all nodes."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-#run_list()
+run_list("recipe[chef::delete_validation]")
 
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
