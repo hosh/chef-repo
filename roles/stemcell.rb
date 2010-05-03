c4ca9f59a0bdec677990af650e6977c5a68565b5
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-05-03 15:04:26 -0400

    [ROLES] Updated stemcell role

--- roles/stemcell.rb
+++ roles/stemcell.rb
@@ -1,7 +1,7 @@
 name "stemcell"
 description "Base role applied to all nodes."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("recipe[chef::delete_validation]")
+run_list("recipe[rackspace::hosts]")
 
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
